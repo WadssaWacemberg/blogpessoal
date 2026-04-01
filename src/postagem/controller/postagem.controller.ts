a021766443e98eac47e911d06b5ef663d65b2ab7
Wadssa Wacemberg <seu-email-do-github@exemplo.com>
2026-04-01 10:15:09 -0300

    feat: implementa metodos CRUD e busca por titulo no service e controller

--- src/postagem/controller/postagem.controller.ts
+++ src/postagem/controller/postagem.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, HttpStatus, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Body,
+  Post,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { PostagemService } from '../services/postagem.service';
 import { Postagem } from '../entity/postagem.entity';
 
@@ -10,5 +21,36 @@ export class PostagemController {
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  // 2. Definido o retorno como Promise<Postagem>
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    // 3. Chamada correta do método do Service passando o ID
+    return this.postagemService.findById(id);
+  }
+
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    return this.postagemService.findByTitulo(titulo);
+  }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.create(postagem);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.postagemService.delete(id);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.update(postagem);
   }
 }
